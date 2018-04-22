51dc2c990f41e57d5b9ff3063af12cd506d4fb62
Sophia <sophiaaa@garagescript.org>
2018-04-22 14:55:23 -0700

    solve js1 #8: greatest common denominator

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -5,8 +5,9 @@
  * @returns {number}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a, b, i= Math.min(a,b)) => {
+  if (Math.min(a,b) % i === 0 && 0 === Math.max(a,b) % i) return i;
+  return solution(a, b, i-1);
 };
 
 module.exports = {
