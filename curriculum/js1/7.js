529ddf66197aafabb025f04c0fe2e87a7bdcf13f
Sophia <sophiaaa@garagescript.org>
2018-04-22 14:30:49 -0700

    solve js1 #7: prime number

--- curriculum/js1/7.js
+++ curriculum/js1/7.js
@@ -5,8 +5,11 @@
  * @returns {boolean}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (a, b=2) => {
+  if (a === 1) return false;
+  if (a === b) return true;
+  if ((a % b) === 0) return false;
+  return solution(a, b+1);
 };
 
 module.exports = {
