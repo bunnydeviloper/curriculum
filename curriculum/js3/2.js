f2069f171009cc2a2ed113aa5dafda0242084eb2
Sophia <sophiaaa@garagescript.org>
2018-04-23 17:16:23 -0700

    solve js3 #2: most common value of array using object helper

--- curriculum/js3/2.js
+++ curriculum/js3/2.js
@@ -4,10 +4,20 @@
  * @param return {number}
  */
 
-const solution = (arr)=>{
-  return 0;
-}
+const solution = (arr, i=0, obj={}, objKey)=>{
+  if (i === arr.length) return obj;
+  if (i < arr.length) {
+    objKey = arr[i];
+    // if key doesn't exist, value will be 0
+    // (This is prevention for falsy, 0, '', NaN, or undefined)
+    if (!obj[objKey]) obj[objKey] = 0;
+  }
+  obj[objKey] += 1;
+  solution(arr, i+1, obj, objKey);
+  const c = Math.max(...Object.values(obj));
+  return +Object.keys(obj).filter( (k) => obj[k] === c);
+};
 
 module.exports = {
-  solution
-}
+  solution,
+};
